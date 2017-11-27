96414278d772b438efb5871f9c7b8515fa7f9c01
Patrick Su <simpleblob@gmail.com>
2017-11-27 23:03:44 +0900

    new posts, some drafts

--- myblog/site.hs
+++ myblog/site.hs
@@ -7,9 +7,6 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
 
     match "assets/*" $ do
         route   idRoute
